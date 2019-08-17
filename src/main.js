bb1efe62c02f3125f29bb6f6859288dcd077d72e
Han Xiao <yhxiao97@gmail.com>
2019-08-17 17:13:47 +0200

    adding skeleton

--- src/main.js
+++ src/main.js
@@ -10,23 +10,31 @@ require('app-module-path').addPath(rootPath);
 const config = require('src/util/config.js');
 const logger = require('src/util/logger.js');
 
+// Eventually refactor when migrating to K8 so that every pod is a worker with a master pod
+//  instead of using cluster module from javascript
 const handleWorker = () => {
     switch(process.env.workerType){
         case 'pool':
             break;
     }
-}
+};
 
 const main = () => {
     if (cluster.isWorker){
         handleWorker();
     }
-}
+    else if (config.get('pool:singleThread')){
+
+    }
+    else {
+
+    }
+};
 
 main() 
 .cath(err => {
     logger.core.error(err);
     process.exit(1);
-}) ;
+});
 
 
