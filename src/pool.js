37f9190e8c1d411f26b9cc4a99e2fadb5be94097
Han Xiao <yhxiao97@gmail.com>
2019-08-18 13:36:46 +0200

    restructure p1

---
+++ src/pool.js
@@ -0,0 +1,8 @@
+// Pool TCP server
+
+
+const PoolTCPServer = {
+
+}
+
+module.exports = PoolTCPServer;
--- src/util/spawner.js
+++ src/util/spawner.js
@@ -1,10 +1,10 @@
-const PoolService = require('src/services/pool.service.js');
-const spawner = {
-    pool: PoolService.spawn,
-    all: () => {
-        PoolService.spwan()
-    }
+// const PoolService = require('src/services/pool.service.js');
+// const spawner = {
+//     pool: PoolService.spawn,
+//     all: () => {
+//         PoolService.spawn()
+//     }
 
-}
+// }
 
 module.exports = spawner;
