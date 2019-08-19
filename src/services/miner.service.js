ad16ddffcb3424ba3c8bdfb9a04fc596bae9f479
Han Xiao <yhxiao97@gmail.com>
2019-08-19 18:16:50 +0200

    [PS01] Adding the pool.js server file

---
+++ src/services/miner.service.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const MinerService = {
+
+    getJob: () => {
+        return new Promise((resolve, reject) => {
+            reject(server.error(501, 'not implemented'));
+        });
+    },
+
+    submit: () => {
+        return new Promise((resolve, reject) => {
+            reject(server.error(501, 'not implemented'));
+        });
+    },
+
+    login: () => {
+        return new Promise((resolve, reject) => {
+            reject(server.error(501, 'not implemented'));
+        });
+    }
+}
+
+module.exports = MinerService;
