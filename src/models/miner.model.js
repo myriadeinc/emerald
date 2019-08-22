b885f9f936a70d64b9acfcafbf20e3f7c1ac7d41
Han Xiao <yhxiao97@gmail.com>
2019-08-22 21:23:56 +0200

    PS07 Adding skeleton for Miner model

---
+++ src/models/miner.model.js
@@ -0,0 +1,11 @@
+const logger = require('src/util/logger.js').miner;
+const config = require('src/util/config.js');
+
+const cryptonoteUtils = require('src/util/cryptonote.js')
+
+class MinerModel {
+
+    
+}
+
+export default MinerModel;
