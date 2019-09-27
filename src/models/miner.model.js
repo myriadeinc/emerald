aaf1f74269fa87759f38aa4e546aa2710cd008ec
andyjung0 <hangyeol.jung@mail.mcgill.ca>
2019-09-27 17:12:51 -0400

    PS-08 fix suggestion

--- src/models/miner.model.js
+++ src/models/miner.model.js
@@ -6,8 +6,14 @@ const _ = require('lodash');
 
 const VarDiff = require('src/util/global.js').VARDIFF;
 
+// Replace with rabbitMQ
+const rmq = function(){};
+
 const DiamondApi = require('src/api/diamond.api.js');
 const diamondApi = new DiamondApi();
+MoneroApi = require('src/monero.api');
+const moneroApi = new MoneroApi();
+
 const cache = require('src/util/cache.js');
 
 class MinerModel {
@@ -51,6 +57,81 @@ class MinerModel {
         }
     }
 
+
+    async recordShareData(blob){
+            rmq.push(blob);
+    }
+
+    submit(miner, job, blockTemplate, params){
+          // From nodejs pool
+          let nonce = params.nonce;
+          let resultHash = params.result;
+          let template = Buffer.alloc(blockTemplate.buffer.length);
+          
+          blockTemplate.buffer.copy(template);
+          template.writeUInt32BE(job.extraNonce, blockTemplate.reserveOffset);
+          /**
+           * Left over in case we introduce miner proxy functionality (not documented)
+           * blockTemplate.buffer.copy(template);
+              template.writeUInt32BE(job.extraNonce, blockTemplate.reserveOffset);
+              template.writeUInt32BE(params.poolNonce, job.clientPoolLocation);
+              template.writeUInt32BE(params.workerNonce, job.clientNonceLocation);
+           * 
+           * 
+           */
+          cnBlobType = {};
+          // utils.cnUtil -> cryptonote.cnutil
+          let shareBuffer = cryptonote.cnutil.construct_block_blob(template, Buffer.from(nonce, 'hex'), cnBlobType);
+      
+          let convertedBlob;
+          let hash;
+    
+          convertedBlob = cryptonote.cnutil.convert_blob(shareBuffer, cnBlobType);
+              let hard_fork_version = convertedBlob[0];
+      
+              // For randomX
+                  hash = cryptonote.cnutil.cryptoNight(convertedBlob, Buffer.from(blockTemplate.seed_hash, 'hex'), cnVariant);
+      
+          
+      
+          if (hash.toString('hex') !== resultHash) {
+            // miner has Bad hash 
+              log('warn', logSystem, 'Bad hash from miner');
+              return false;
+          }
+      
+          let hashArray = hash.toByteArray().reverse();
+          let hashNum = bignum.fromBuffer(Buffer.from(hashArray));
+          let hashDiff = diff1.div(hashNum);
+          // time sensitive so cannot promisify right away
+          if (hashDiff.ge(blockTemplate.difficulty)){
+            // call monero rpc submit block, share buffer will be converted to hex string
+             moneroApi.submitblock(shareBuffer, function(error, result){
+                  if (error){
+                    // Log error
+                      recordShareData(Set([miner, job, hashDiff.toString()]));
+                  }
+                  else{
+                      let blockFastHash = cryptonote.get_block_id(shareBuffer, cnBlobType).toString('hex');
+                      // Log success
+                      recordShareData(Set([miner, job, hashDiff.toString(), true, blockFastHash, shareType, blockTemplate]));
+                      // Link this function !!
+                      jobRefresh();
+                      return true;
+                  }
+              });
+          }
+      
+          else if (hashDiff.lt(job.difficulty)){
+              // Log low difficulty, no share granted
+              return ;
+          }
+          else{
+              recordShareData(Set([miner, job, hashDiff.toString()]));
+          }
+          return false;
+
+    }
 
 
     getJob() {
