d4d570464b95e15fd7826a958dd421fbf22d6488
Andy Jung <44798950+andyjung-dev@users.noreply.github.com>
2019-11-01 21:32:05 -0400

    PS-20 Code Completion for Emerald including submit() and get_job() functionality (#9)

--- src/models/miner.model.js
+++ src/models/miner.model.js
@@ -1,13 +1,39 @@
 const logger = require('src/util/logger.js').miner;
 const config = require('src/util/config.js');
 const err = require('src/util/error.js');
-const cryptonoteUtils = require('src/util/cryptonote.js');
 const _ = require('lodash');
+const globals = require('src/util/global.js');
 
-const VarDiff = require('src/util/global.js').VARDIFF;
+const cryptoNoteUtils = require('src/util/cryptonote.js');
+/**
+ * 
+ * Currently broken, we will need to fix
+ */
+const multiHash = require('mutli-hashing');
+const cryptoNight = mutliHash['cryptonight'];
+function cryptoNightFast(buf){
+    return cryptoNight(Buffer.concat([new Buffer([buf.length]), buf]), true);
+}
+
+const VarDiff = globals.VARDIFF;
+const diff = globals.refDiff;
+
+const bignum = require('bignum');
+
+const JobHelperService = require('src/services/job.helper.service.js');
+const BlockTemplateService = require('src/services/block.template.service.js');
 
 const DiamondApi = require('src/api/diamond.api.js');
 const diamondApi = new DiamondApi();
+
+const MoneroApi = require('src/api/monero.api.js');
+const moneroApi = new MoneroApi();
+
+const SapphireApi = require('src/api/monero.api.js');
+const sapphireApi = new SapphireApi();
+
+//key value job 
+
 const cache = require('src/util/cache.js');
 
 class MinerModel {    
@@ -36,23 +62,70 @@ class MinerModel {
     }
 
     /**
+     * 
      * @returns {BlockTemplate} jobTemplate
      */
     getJob() {
-        return new Promise((resolve, reject) => {
-            resolve('Boilerplate for Miner::getJob');
-        })
+        job = JobHelperService.create(BlockTemplateService.getBlock();
+        return {
+            job_id: job.job_id,
+            params: {
+                blob: job.blob
+            }
+        }
     }
-
+    
     /**
-     * @param {BlockTemplate} blockTemplate 
-     */
-    submit(blockTemplate) {
-        return new Promise((resolve, reject) => {
-            resolve('Boilerplate for Miner::submit');
-        })
+     * 
+     * @Example Request
+     * "id": 2,
+     * "jsonrpc": "2.0",
+     * "method": "submit",
+     * "params": {
+     * "id": "1be0b7b6-b15a-47be-a17d-46b2911cf7d0",
+     * "job_id": "4BiGm3/RgGQzgkTI/xV0smdA+EGZ",
+     * "nonce": "d0030040",
+     * "result": "e1364b8782719d7683e2ccd3d8f724bc59dfa780a9e960e7c0e0046acdb40100"
+     * }
+     * @param {request} req
+     */ 
+    submit(req) {
+        /*
+            minerdata.id = miner uuid
+            minerdata.job_id = job id
+            minerdata.nonce = nonce
+            minerdata.result = resultHash -> the hash of the supposed block
+            Reconstruct the block from data,check to see that it matches the sent block
+            */
+        const minerData = 
+            {
+                "id": req.params.id,
+                "job_id": req.params.job_id,
+                "nonce": req.params.nonce,
+                "result": req.params.result
+            };
+        // Get the current block template and create a block with the provided nonce
+        var block = BlockReferenceService.buildBlock(minerData);
+        var job = JobHelperService.getFromId(minerData.job_id);
+
+        // Convert block into blob, then hash with randomx and compare with the hash that the miner sent
+        if(!BlockReferenceService.checkBlock(block, minerData.result)){
+            sapphireApi.sendShareInfo("Bad Block hash provided, penalty to miner");
+            return {};
+        }
+        // If the difficulty check passes we submit the block
+        if(BlockReferenceService.checkDifficulty(job.difficulty,block)){
+            return moneroApi.submit(block)
+            .then((result) => {
+                sapphireApi.sendShareInfo("good block provided, share given to miner");
+                return null;
+            });
+        }
+        else{
+            sapphireApi.sendShareInfo();
+            return {error: "does not meet difficulty"};
+        }
     }
 }
-
 
 module.exports = MinerModel;
---
+++ src/services/block.reference.service.js
@@ -0,0 +1,90 @@
+const config = require('src/util/config.js');
+const logger = require('src/util/logger.js').block;
+const err = require('src/util/error.js').BlockReference;
+
+const xmrUtil = require('cryptoforknote-util');
+const multiHashing = require('cryptonight-hashing');
+
+const blockTemplateService = require('src/services/block.template.service.js');
+
+
+/**
+ * @Note
+ * @description
+ * This model was created to separate the block processing logic from miner model
+ * 
+ */
+const BlockReferenceService = {
+    buildBlock: (minerData, job) => {
+        try{
+            const job = jobHelperService.getFromId(minerData.job_id);
+
+            var block = new Buffer(blockTemplate.buffer.length);
+            blockTemplate.buffer.copy(this.block);
+            // Write the extra nonce first, then the regular nonce
+            this.block.writeUInt32BE(job.extraNonce, blockTemplate.reserveOffset);
+            /* 
+            For fallback:
+            new Buffer(minerData.nonce, 'hex').copy(block, 39);
+            Writing the nonce in a specific position if util does not work in testing
+            */
+            const randomXid = 1;
+            const NonceBuffer = new Buffer(minerData.nonce,'hex');
+            return xmrUtil.construct_block_blob(blockTemplate, NonceBuffer, randomXid);
+            
+
+        }
+        catch(e){
+            logger.error(e);
+            throw err.instantiation;
+        }
+    },
+    checkBlock: (block, result) => {
+        const blockHashed = xmrUtil.hash(block, "randomx");
+        
+        if(blockHashed.toString('hex') !== minerdata.result){
+            logger("Bad Hash!!!1");
+            return false;
+        }
+
+        return true;
+
+    },
+
+    checkDifficulty: (difficulty, blockHashed) => {
+        var hashArray = blockHashed.toJSON().reverse();
+
+        // Diff is a reference from bignum 
+        var hashDiff = diff.div(bignum.fromBuffer(new Buffer(hashArray)));
+    
+        if (hashDiff.ge(blockTemplate.difficulty)){
+            moneroApi.submit(block, function(error, result){
+                if (error){
+                    sapphireApi.sendShareInfo();
+                    resolve("MONERO API ERROR");
+                    return false;
+                }
+                else{
+                    var blockFastHash = cryptoNightFast(convertedBlob || cryptoNoteUtils.cnUtil.convert_blob(block)).toString('hex');
+                    // Send blockfasthash instead
+                    sapphireApi.sendShareInfo();                        
+                    jobRefresh();
+                    resolve("Share Granted");
+                    return true;
+                }
+            });
+        }
+    
+        else if (hashDiff.lt(job.difficulty)){
+            // Miner sent bad block/nonce, this is a bannable offense since XMRig should not be sending any below target difficulty
+            sapphireApi.sendShareInfo();
+            return false;
+        }
+
+    },
+
+
+
+};
+
+module.exports = BlockReferenceService;
